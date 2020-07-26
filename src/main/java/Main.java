a209e019ab207c240879c94417c02471db5c5b06
Amit Kulkarni <amit.kulkarni@sunbeaminfo.com>
2020-07-26 20:52:40 +0530

    initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!!!");
+    }
+
+    public static int add(int x, int y) {
+        return x + y;
+    }
+}
