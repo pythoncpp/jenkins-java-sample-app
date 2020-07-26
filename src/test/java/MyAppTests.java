065d2650764153e405d53a8ea02f8fa3fd0916e3
Amit Kulkarni <amit.kulkarni@sunbeaminfo.com>
2020-07-26 21:26:36 +0530

    fixed the test case for multiplication

--- src/test/java/MyAppTests.java
+++ src/test/java/MyAppTests.java
@@ -13,7 +13,7 @@ public class MyAppTests {
 
     @Test
     public void multiplyTest() {
-        int answer = Main.add(20, 40);
+        int answer = Main.multiply(20, 40);
 
         // the answer must be 60
         Assert.assertEquals(answer, 800);
