8ccb5234bc435d51ddc437c1e1f62ff9e9b082ca
Amit Kulkarni <amit.kulkarni@sunbeaminfo.com>
2020-07-26 21:13:48 +0530

    added a test case for testing add method

---
+++ src/test/java/MyAppTests.java
@@ -0,0 +1,13 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MyAppTests {
+
+    @Test
+    public void addTest() {
+        int answer = Main.add(20, 40);
+
+        // the answer must be 60
+        Assert.assertEquals(answer, 60);
+    }
+}
